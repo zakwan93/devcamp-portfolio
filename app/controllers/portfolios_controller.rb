822e831adf90ac222f9b84b77f2ef3e28d4cdc54
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-30 14:57:16 -0400

    Refectored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show,:edit,:update, :destroy]
 
   def index
     @portfolio_items = Portfolio.all
@@ -9,7 +10,7 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end
 
   def new
@@ -18,7 +19,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -30,13 +31,13 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
+    
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "Portfolio Item is updated"}
       else
         format.html { render :edit}
@@ -45,7 +46,7 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
+    
     @portfolio_item.destroy
     respond_to do |format|
       format.html {redirect_to portfolios_path, notice: "Portfolio item was removed"}
@@ -55,4 +56,18 @@ class PortfoliosController < ApplicationController
   def missing
 
   end
+
+  private
+
+    def set_portfolio
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle, 
+                                        :body, 
+                                        technologies_attributes: [:name]
+                                       )
+    end
 end
