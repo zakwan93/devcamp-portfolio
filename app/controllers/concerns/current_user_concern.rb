755de4e0e580250016941b8b8c68a6459ff26f55
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-30 15:26:13 -0400

    Added guest user for current user

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,19 @@
+module CurrentUserConcern
+   extend ActiveSupport::Concern
+
+  included do 
+    before_action :current_user
+  end
+
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: "Guest User", 
+                  first_name: "Guest", 
+                  last_name: "User", 
+                  email: "guest@example.com"
+                  )
+  end
+end
